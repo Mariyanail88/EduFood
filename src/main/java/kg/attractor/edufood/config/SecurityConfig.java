3f70848089f2315d1fb230e063ac7a78553bd26e
Mariya <mariasherbakiva@mail.ru>
2024-09-10 16:40:19 +0600

    configs

---
+++ src/main/java/kg/attractor/edufood/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package kg.attractor.edufood.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+//                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection if you want to disable it
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login")
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/auth/login?error=true")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/") // Redirect to homepage after logout
+                        .permitAll())
+                .authorizeHttpRequests(request -> request
+//                        .requestMatchers("/").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/movies").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
+                        .requestMatchers("/create").hasAuthority("ADMIN")
+                        .anyRequest().permitAll());
+        return http.build();
+    }
+}
